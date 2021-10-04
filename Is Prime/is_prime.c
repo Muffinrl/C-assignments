b14670da5651b1efb4b18f58952644519211b2f5
Joshua van der Geize <mrmuffincake232@gmail.com>
2021-10-04 13:24:24 +0200

    Added user input handling. Small edits to isPrime()
    1 was being handled as a prime number, added check at return statement. Not the most efficient, but the cleanest way in my opinion.

--- Is Prime/is_prime.c
+++ Is Prime/is_prime.c
@@ -8,12 +8,18 @@
 int isPrime(int a);
 
 int main(void) {
-    // Values for testing, correct output should be: 1 1 0 1 1
-    printf("2 is prime: %d\n", isPrime(2));
-    printf("3 is prime: %d\n", isPrime(3));
-    printf("4 is prime: %d\n", isPrime(4));
-    printf("5 is prime: %d\n", isPrime(5));
-    printf("13 is prime: %d\n", isPrime(13));
+    int input;
+
+    printf("Enter a natural number: ");
+    scanf("%d", &input);
+
+    if(isPrime(input)) {
+        printf("%d is prime!", input);
+    }
+    else {
+        printf("%d is not prime!", input);
+    }
+
     return 0;
 }
 
@@ -22,12 +28,13 @@ int main(void) {
     it proceeds to go through all its divisors, adding every "succesful" division to the auxiliary variable c.
     c starts at 0, meaning that if a has 2 divisors, c will be 1, because the input value itself isn't counted as a divisor.
     Thus, the output of isPrime() will be c == 1.
+    a = 1 will still return true for c == 1, so it is handled seperately.
 */
-int isPrime(int a){
+int isPrime(int a) {
     int c = 0;
     for (int i = 1; i <= sqrt(a); i++) {
         if (!fmod(a, i))
             c++;
     }
-    return c == 1;
+    return c == 1 && a != 1;
 }
