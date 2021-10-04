3e6e39464a26369a10607ca70c7369adce23ad01
Joshua van der Geize <mrmuffincake232@gmail.com>
2021-10-04 21:28:06 +0200

    Finished gcd.c

--- GCD/gcd.c
+++ GCD/gcd.c
@@ -9,14 +9,32 @@
 int gcd(int p, int q);
 
 int main(void) {
-    printf("%d", gcd(15, 6));
+    int a, b; /* Inputs */
+
+    printf("Enter two numbers:\n");
+    scanf("%d\n%d", &a, &b);
+    printf("The GCD of %d and %d\n%d", a, b, gcd(a,b));
     return 0;
 }
 
+/*
+    gcd() is an iterative version of the recursive euclidian algorithm for finding the GCD.
+    gcd() takes two inputs p,q and stores the modulus p%q in the auxiliary variable r.
+    Checking for r == 0 as a means to find the common denominator.
+    As we are going down from the top posssible value,
+    that being the lowest of p and q, the first case for r == 0 will be the GCD.
+*/
 int gcd(int p, int q) {
-    int r = p % q;
-    while(r != 0) {
-        r = q % r;
+    int r;
+    if ((r = p % q) == 0) {
+        return q;
     }
-    return q;
+    else {
+        while(r != 0) {
+            p = q;
+            q = r;
+            r = p % q;
+        }
+        return q;
+    }
 }
