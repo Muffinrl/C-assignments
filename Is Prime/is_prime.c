9ae5c44bccb54ee8cde2cf22d128b27fb487a2d7
Joshua van der Geize <mrmuffincake232@gmail.com>
2021-10-04 13:04:46 +0200

    isPrime() complete, maybe?
    Not yet tested

--- Is Prime/is_prime.c
+++ Is Prime/is_prime.c
@@ -3,6 +3,7 @@
 */
 
 #include <stdio.h>
+#include <math.h>
 
 int main(void) {
     return 0;
@@ -10,10 +11,15 @@ int main(void) {
 
 /*
     isPrime() takes an input value a of type int.
-    it proceeds to go through all its divisors, adding every "succesfull" division to the auxiliary variable i.
-    i starts at -2, meaning that if a has 2 divisors, i will be 0.
-    This is a clean way to check for 2 divisors. As 0 corresponds with false, thus the output will be i == 0.
+    it proceeds to go through all its divisors, adding every "succesful" division to the auxiliary variable c.
+    c starts at 0, meaning that if a has 2 divisors, c will be 2.
+    Thus, the output of isPrime() will be c == 2.
 */
 int isPrime(int a){
-
+    int c = 0;
+    for (int i = 0; i < sqrt(a); i++) {
+        if (!fmod(a, i))
+            c++;
+    }
+    return c == 2;
 }
