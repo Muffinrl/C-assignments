05fea5a7e42f6a281c2bdb23038ef7e04cce12cd
Joshua van der Geize <mrmuffincake232@gmail.com>
2021-10-04 12:19:47 +0200

    Setting up is_prime.c
    Mainly boilerplate and some comments to lay out the plan of attack.

---
+++ Is Prime/is_prime.c
@@ -0,0 +1,19 @@
+/*
+    This program corresponds to assignment 2.12: The program is_prime.
+*/
+
+#include <stdio.h>
+
+int main(void) {
+    return 0;
+}
+
+/*
+    isPrime() takes an input value a of type int.
+    it proceeds to go through all its divisors, adding every "succesfull" division to the auxiliary variable i.
+    i starts at -2, meaning that if a has 2 divisors, i will be 0.
+    This is a clean way to check for 2 divisors. As 0 corresponds with false, thus the output will be i == 0.
+*/
+int isPrime(int a){
+
+}
